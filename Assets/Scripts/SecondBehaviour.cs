d90c095629bc12f1bbc7780729a66ed7c97d9aff
KostasBan <mpanakak@hotmail.com>
2022-02-19 18:30:45 +0200

    Adds a second behaviour

---
+++ Assets/Scripts/SecondBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("I am just a second Behaviour");       
+    }
+}
